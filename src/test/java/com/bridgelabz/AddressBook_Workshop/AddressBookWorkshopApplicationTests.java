34d203561bd6aba394fe67aa3136b15dea6c51ea
Shashankkumar-github <101034283+Shashankkumar-github@users.noreply.github.com>
2025-03-10 20:44:50 +0530

    Spring initializr

---
+++ src/test/java/com/bridgelabz/AddressBook_Workshop/AddressBookWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.AddressBook_Workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
